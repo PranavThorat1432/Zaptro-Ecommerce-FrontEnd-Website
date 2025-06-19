9cdc6fd1f9b99575d8d4109e19632b73b87d4397
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-19 16:56:41 +0530

    Wrapped Code

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { DataProvider } from './Context/DataContext.jsx'
+import { CartProvider } from './Context/CartContext.jsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -13,11 +14,13 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <DataProvider>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-        <App />
-      </ClerkProvider>
+      <CartProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+          <App />
+        </ClerkProvider>
+      </CartProvider>
     </DataProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
