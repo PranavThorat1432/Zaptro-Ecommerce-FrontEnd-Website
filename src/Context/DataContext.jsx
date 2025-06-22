2f75e8956b634a5537a69bc7de11a6daeb3fddde
PranavThorat1432 <pranavthorat95@gmail.com>
2025-06-22 17:29:18 +0530

    Data Context

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -6,23 +6,27 @@ export const DataContext = createContext(null);
 
 export const DataProvider = ({children}) => {
     const [data, setData] = useState()
+    const [loading, setLoading] = useState(true)
 
     // fetching all products from api
     const fetchAllProducts = async () => {
+        setLoading(true)
         try {
             const res = await axios.get("https://fakestoreapi.in/api/products?limit=150")
-            
+            console.log(res);
             const productsData = res.data.products;
             setData(productsData)
             
         } catch (error) {
             console.log(error);
             
+        } finally {
+            setLoading(false)
         }
     }
 
     const getUniqueCategory = (data, property) => {
-        let newVAL =data?.map((curElem) => {
+        let newVAL = data?.map((curElem) => {
             return curElem[property]
         })
         return ["All",...new Set(newVAL)]
@@ -31,7 +35,7 @@ export const DataProvider = ({children}) => {
     const categoryOnlyData = getUniqueCategory(data, 'category')
     const brandOnlyData = getUniqueCategory(data, 'brand')
 
-    return <DataContext.Provider value={{data, setData, fetchAllProducts, categoryOnlyData, brandOnlyData}}>
+    return <DataContext.Provider value={{data, loading, setData, fetchAllProducts, categoryOnlyData, brandOnlyData}}>
         {children}
     </DataContext.Provider>
 }
